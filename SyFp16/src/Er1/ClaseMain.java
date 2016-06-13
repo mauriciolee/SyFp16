5c22b77783bd3fc9865e9d2acc13bbe9ad3be730
danylee <danylee@10.20.7.118>
2016-06-13 11:42:35 -0500

    examen

---
+++ SyFp16/src/Er1/ClaseMain.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Er1;
+
+import E6.Convertidor;
+
+/**
+ *
+ * @author danylee
+ */
+public class ClaseMain {
+    public static void main (String[]args){
+        ClaseMain j = new ClaseMain();
+        ClaseMain a = new ClaseMain();
+        2 c = new ClaseMain();
+        
+        
+        
+        
+        
+    }
+
+    
+    
+}
